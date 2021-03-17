f43e3a258c45cebd56aded8e185bac52c5f623b9
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-03-17 12:34:09 +0800

    Revert "Add Firestore"
    This reverts commit a1a496018f055a7b1d38b8702fec6c866a080539.

--- app/src/main/java/com/example/mad_assignment/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment/MainActivity.kt
@@ -1,41 +1,11 @@
 package com.example.mad_assignment
 
-import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.FirebaseFirestore
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val createUserBtn: Button = findViewById(R.id.btn_createUser)
-
-        createUserBtn.setOnClickListener {
-            val username = "lky1020"
-            val password = "lky1020"
-
-            createUser(username, password)
-        }
-    }
-
-    private fun createUser(username: String, password: String){
-        val db = FirebaseFirestore.getInstance()
-
-        val user: MutableMap<String, Any> = HashMap()
-        user["username"] = username
-        user["password"] = password
-
-        db.collection("users").document(username)
-                .set(user)
-                .addOnSuccessListener {
-                    Toast.makeText(this, "Added Successfully", Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener() {
-                    Toast.makeText(this, "Added Unsuccessfully", Toast.LENGTH_SHORT).show()
-                }
-
     }
 }
