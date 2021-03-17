2a1656c9fa99734f41e0461a29cd679cc1ab424e
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-03-17 13:21:28 +0800

    Add Realtime Database

---
+++ app/src/main/java/com/example/mad_assignment/Class/User.kt
@@ -0,0 +1,3 @@
+package com.example.mad_assignment.Class
+
+data class User (val username: String, val password:String)
--- app/src/main/java/com/example/mad_assignment/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.mad_assignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mad_assignment.Class.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.lang.StringBuilder
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.reference.child("User")
+
+        val btnCreate: Button = findViewById(R.id.btn_createUser)
+
+        btnCreate.setOnClickListener(){
+            val user = User("testing1", "testing1")
+            myRef.push().setValue(user)
+        }
+
+        var getData = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var sb = StringBuilder();
+
+                for(c in snapshot.children){
+                    var name = c.child("username").getValue()
+                    sb.append("${name}\n")
+                }
+
+                val tvTest: TextView = findViewById(R.id.tvTest)
+                tvTest.setText(sb)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        }
+
+        val btnGet: Button = findViewById(R.id.btn_getUser)
+
+        btnGet.setOnClickListener(){
+            myRef.addValueEventListener(getData)
+            myRef.addListenerForSingleValueEvent(getData)
+        }
+
     }
 }
