43e162f435976a8d0ded2dbe436e5263da3a0c1d
Joan0018 <joanhau201018@hotmail.com>
2021-03-23 16:42:20 +0800

    Update Change Language

--- app/src/main/java/com/example/mad_assignment/Cust_Staff_Shared/Cust_Staff_Fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mad_assignment/Cust_Staff_Shared/Cust_Staff_Fragments/profile/ProfileFragment.kt
@@ -9,7 +9,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.example.mad_assignment.R
 
 //belongs to fragment_profile.xml
