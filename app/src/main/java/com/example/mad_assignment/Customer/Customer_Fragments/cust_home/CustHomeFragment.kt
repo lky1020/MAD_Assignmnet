921dfc184355d96cd91fd289b41be092e0c768c6
YinLamCheong <cheongyl-wm18@student.tarc.edu.my>
2021-04-19 03:42:24 +0800

    report list page & user report

--- app/src/main/java/com/example/mad_assignment/Customer/Customer_Fragments/cust_home/CustHomeFragment.kt
+++ app/src/main/java/com/example/mad_assignment/Customer/Customer_Fragments/cust_home/CustHomeFragment.kt
@@ -1,21 +1,31 @@
 package com.example.mad_assignment.Customer_Fragments.cust_home
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mad_assignment.Customer.Booking.Class.Reservation
 import com.example.mad_assignment.Customer.Customer_Fragments.cust_home.CustHomeAdapter
 import com.example.mad_assignment.Customer.Customer_Fragments.cust_home.Cust_Home_Item
+import com.example.mad_assignment.Customer.Customer_Fragments.cust_trans_history.payment.cust_payment_transaction_details
 //import com.example.mad_assignment.Customer.Customer_Fragments.cust_housekeeping.Main.CustHousekeepingFragment
 //import com.example.mad_assignment.Customer_Fragments.cust_facilities.CustFacilitiesFragment
 import com.example.mad_assignment.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.customer_fragment_home.*
 
 //belongs to customer_fragment_home.xml
@@ -41,10 +51,8 @@ class CustHomeFragment : Fragment() {
 
         home_items.add(Cust_Home_Item("Room", R.drawable.banner_hotelroom, "We have many room type which single rooms to quad rooms, queen rooms, king rooms, and president suite.", "Book Room >>"))
         home_items.add(Cust_Home_Item("Facilities", R.drawable.banner_hotelfacility, "There are many facilities such as spa, swimming pool, gym room and etc. Waiting you to enjoy it!", "Book Facility >>"))
-        //need modify the 3rd der
-        if(isbooking) {
-            home_items.add(Cust_Home_Item("Services", R.drawable.banner_hotelroom, "Let's book our services! We will provide the best services to you.", "Book Services >>"))
-        }
+        home_items.add(Cust_Home_Item("Services", R.drawable.banner_hotelroom, "Let's book our services! We will provide the best services to you.", "Book Services >>"))
+
 
         return root
     }
@@ -56,6 +64,7 @@ class CustHomeFragment : Fragment() {
         recyclerView.adapter = CustHomeAdapter(requireActivity(),home_items) //Initialize adapter
 
     }
+
 }
 
 
--- app/src/main/java/com/example/mad_assignment/Staff/Staff_Fragments/manager_report/ManagerReportFragment.kt
+++ app/src/main/java/com/example/mad_assignment/Staff/Staff_Fragments/manager_report/ManagerReportFragment.kt
@@ -1,32 +1,98 @@
 package com.example.mad_assignment.Staff_Fragments.manager_report
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.mad_assignment.Class.User
+import com.example.mad_assignment.Cust_Staff_Shared.Cust_Staff_Fragments.Login
 import com.example.mad_assignment.R
+import com.example.mad_assignment.Staff.Staff_Fragments.StaffHomeFragment
+import com.example.mad_assignment.Staff.Staff_Fragments.manager_report.UserReportFragment
+import com.example.mad_assignment.Staff.Staff_Fragments.staff_checkInOut.Main.StaffCheckInOutMainActivity
+import com.example.mad_assignment.Staff.Staff_Fragments.staff_housekeeping.Main.Main.StaffHousekeepingMainActivity
+import com.example.mad_assignment.Staff.Staff_Fragments.staff_housekeeping.Main.Main.StaffHousekeepingMainFragment
+import com.example.mad_assignment.Staff.Staff_Fragments.staff_manager.Main.Staff.StaffManagerActivity
+import com.example.mad_assignment.Staff.room.Main.ManageRoomMenu
+import java.time.LocalDate
 
 //belongs to staff_fragment_home.xml
 class ManagerReportFragment : Fragment() {
-
-    private lateinit var managerReportViewModel: ManagerReportViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        managerReportViewModel =
-                ViewModelProvider(this).get(ManagerReportViewModel::class.java)
+
         val root = inflater.inflate(R.layout.staff_fragment_report, container, false)
-        val textView: TextView = root.findViewById(R.id.text_report)
-        managerReportViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //card views
+        val cv_user_report: CardView = view.findViewById(R.id.cv_user_report)
+        val cv_reservation_report: CardView = view.findViewById(R.id.cv_reservation_report)
+        val cv_housekeeping_report: CardView = view.findViewById(R.id.cv_housekeeping_report)
+        val cv_facility_report: CardView = view.findViewById(R.id.cv_facility_report)
+
+
+        //redirect to user report page
+        cv_user_report.setOnClickListener() {
+            val ft: FragmentTransaction = activity?.supportFragmentManager!!.beginTransaction()
+            ft.replace(R.id.nav_host_fragment_staff, UserReportFragment())
+            ft.commit()
+        }
+
+        //*************redirect to reservation report page
+        cv_reservation_report.setOnClickListener() {
+            /*
+            activity?.let{
+                val intent = Intent (it, StaffCheckInOutMainActivity::class.java)
+                intent.putExtra("type", "check out")
+                it.startActivity(intent)
+            }
+
+             */
+        }
+
+        //redirect to housekeeping report page
+        cv_housekeeping_report.setOnClickListener() {
+            /*
+            activity?.let{
+                val intent = Intent (it, StaffHousekeepingMainActivity::class.java)
+                it.startActivity(intent)
+            }
+
+             */
+        }
+
+        //*************redirect to facility report page
+        cv_facility_report.setOnClickListener() {
+            /*
+            activity?.let{
+                val intent = Intent (it, ManageRoomMenu::class.java)
+                it.startActivity(intent)
+            }
+
+             */
+        }
+
     }
 }
