e94b0d36398b5861003e8bcb01e44b6d264b7905
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-04-21 05:43:14 +0800

    Remove Hardcode (Ky)

--- app/src/main/java/com/example/mad_assignment/Customer/Customer_Fragments/cust_housekeeping/Main/CustHousekeepingServiceFragment.kt
+++ app/src/main/java/com/example/mad_assignment/Customer/Customer_Fragments/cust_housekeeping/Main/CustHousekeepingServiceFragment.kt
@@ -78,67 +78,6 @@ class CustHousekeepingServiceFragment : Fragment() {
             recyclerView.adapter = HousekeepingServicesAdapter(it, requireActivity())
             housekeepingList = it
         })
-
-        // Room Cleaning
-//        val servicesList1 = ArrayList<RoomCleaningService>()
-//        servicesList1.add(RoomCleaningService("Apr 24 2021","11:00 AM", "11:30 AM", "Available"))
-//        servicesList1.add(RoomCleaningService("Apr 24 2021","11:30 AM", "12:00 PM", "Available"))
-//        servicesList1.add(RoomCleaningService("Apr 24 2021","12:00 PM", "12:30 PM", "Available"))
-//        servicesList1.add(RoomCleaningService("Apr 24 2021","12:30 PM", "01:00 PM", "Available"))
-//        servicesList1.add(RoomCleaningService("Apr 24 2021","01:00 PM", "01:30 PM", "Available"))
-//
-//        val myRef1 = FirebaseDatabase.getInstance().getReference("Housekeeping").child("Room Cleaning").child("ServicesAvailable")
-//        myRef1.child("Apr 24 2021").setValue(servicesList1)
-//
-//        //Laundry Service
-//        val servicesList2 = ArrayList<LaundryService>()
-//        servicesList2.add(LaundryService("Apr 24 2021","11:00 AM", "03:00 PM", "Available"))
-//        servicesList2.add(LaundryService("Apr 24 2021","11:30 AM", "05:00 PM", "Available"))
-//        servicesList2.add(LaundryService("Apr 24 2021","12:00 PM", "05:00 PM", "Not Available"))
-//        servicesList2.add(LaundryService("Apr 24 2021","12:30 PM", "08:00 PM", "Available"))
-//        servicesList2.add(LaundryService("Apr 24 2021","01:30 PM", "08:00 PM", "Available"))
-//        servicesList2.add(LaundryService("Apr 24 2021","02:00 PM", "03:00 PM", "Available"))
-//        servicesList2.add(LaundryService("Apr 24 2021","03:30 PM", "04:00 PM", "Available"))
-//        servicesList2.add(LaundryService("Apr 24 2021","04:00 PM", "05:00 PM", "Not Available"))
-//        servicesList2.add(LaundryService("Apr 24 2021","05:30 PM", "05:30 PM", "Available"))
-//        servicesList2.add(LaundryService("Apr 24 2021","05:30 PM", "06:00 PM", "Available"))
-//
-//        val myRef2 = FirebaseDatabase.getInstance().getReference("Housekeeping").child("Laundry Service").child("ServicesAvailable")
-//        myRef2.child("Apr 24 2021").setValue(servicesList2)
-
-        //Hardcode db first (waiting staff housekeeping)
-        //Housekeeping
-//        val database = FirebaseDatabase.getInstance()
-//        val myRef = database.getReference("Housekeeping")
-//
-//        val housekeepingItem = Housekeeping("Bed Textiles",
-//                "https://firebasestorage.googleapis.com/v0/b/quadcorehms-5b4ed.appspot.com/o/housekeeping%2FBed%20Textiles?alt=media&token=3bb64699-12d6-4ecf-bebc-45d5ae964165")
-//
-//        myRef.child(housekeepingItem.title).setValue(housekeepingItem)
-//
-//        val filePath: Uri = Uri.parse("res:///" + R.drawable.register_img)
-//
-//        val imageRef = FirebaseStorage.getInstance().getReference("Housekeeping")
-//        imageRef.putFile(filePath)
-
-        //Bed Textiles
-//        val database = FirebaseDatabase.getInstance()
-//        val myRef = database.getReference("Housekeeping").child("Bed Textiles").child("ItemAvailable")
-//
-//        val bedTextilesItem = HousekeepingItem("Cushion",
-//                "https://firebasestorage.googleapis.com/v0/b/quadcorehms-5b4ed.appspot.com/o/bedTextiles%2FCushion.png?alt=media&token=14b11887-220b-4e13-b67b-04874dfa2f1c", 100)
-//
-//        myRef.child(bedTextilesItem.title).setValue(bedTextilesItem)
-
-        //Toiletries
-//        val database = FirebaseDatabase.getInstance()
-//        val myRef = database.getReference("Housekeeping").child("Toiletries").child("ItemAvailable")
-//
-//        val bedTextilesItem = HousekeepingItem("Lotion",
-//                "https://firebasestorage.googleapis.com/v0/b/quadcorehms-5b4ed.appspot.com/o/toiletries%2FLotion.png?alt=media&token=b8cd5f43-1619-4914-a88b-26ffe895718b")
-//
-//        myRef.child(bedTextilesItem.title).setValue(bedTextilesItem)
-
     }
 
     @SuppressLint("DefaultLocale")
